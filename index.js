d79daccc146894ef6a413e2d0251603513e41ea0
DevKayoS <kayoribeirodev@gmail.com>
2024-04-11 12:42:19 -0300

    feat: introduzindo escolhas e imprimindo a escolha

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ const chalk = require('chalk')
 // importação do modulo interno
 const fs = require('fs')
 
-inquirer.prompt([{
-  name: 'name',
-  message: 'Qual é o seu nome?'
-}]).then((answers)=> {
-  console.log(chalk.green(`Seja muito bem vindo ${answers.name}`))
-})
+operation() //chamando a operação para quando o programa seja iniciado ela vir junto
+
+// criando as operações que o sistema pode fazer 
+function operation(){
+
+  inquirer.prompt([
+    {
+    type: 'list', // configurando a forma que as perguntas irão aparecer
+    name:'action', // o nome da variavel onde a resposta vai ficar salva
+    message: 'O que você deseja fazer?', 
+    // escolhas que vão aparecer na lista
+    choices: [
+      'Criar conta',
+      'Consultar saldo',
+      'Depositar',
+      'Sacar',
+      'Sair'
+    ]
+  },
+]).then((answers)=>{
+    const action = answers['action']
+
+    console.log(action)
+
+}).catch((err)=> console.log(err))
+}
 
