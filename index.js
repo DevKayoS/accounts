cc58f04acd218f883b90f8c10183e1912f73d718
DevKayoS <kayoribeirodev@gmail.com>
2024-04-12 14:28:42 -0300

    feat: adicionando a primeira etapa da função de deposito

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ function operation(){
     } else if(action === "Consultar saldo"){ //caso tenha escolhido a opção de consulta de saldo
 
     } else if (action === "Depositar"){ // caso tenha escolhido a opção de depositar dinheiro na conta
-
+      deposit()
     } else if (action === "Sacar"){ // caso tenha escolhido a opção de sacar dinheiro da conta
 
     } else if (action === "Sair"){ // caso usuário tenha escolhido sair da operação
       console.log(chalk.bgBlue("Obrigado por usar nosso banco!")) //mensagem que irá aparecer para o usuário ao sair do programa
-      process.exit()
+      process.exit() //comando para finalizar o programa
     }
 }).catch((err)=> console.log(err)) //catch para caso alguma coisa dê errado seja imprimido a mensagem de erro
 }
@@ -79,4 +79,68 @@ function buildAccount(){
     operation() //apos criar a conta volta para execução para o usuário não sair do programa
   }).catch((err)=> console.log(err)) //verificando se a função teve erro
 
+}
+
+// add an amount to user account (depositando dinheiro na conta do usuário)
+function deposit(){
+
+  inquirer.prompt([{
+    name: 'accountName', //salvando a resposta do usuário em accountName para que possa ser usado depois
+    message: 'Qual o nome da sua conta?'
+  }])
+  .then((answers)=> {
+    const accountName = answers['accountName'] //salvando a resposta em uma variavel
+
+    // verificando se a conta existe
+    if (!checkAccount(accountName)){ 
+      return deposit() // retornando para o começo da função para que o usuário tente novamente
+    }
+
+    inquirer.prompt([{ //salvando a resposta do usuário em ammount
+      name: 'amount',
+      message: 'Quanto você gostaria de depositar?' 
+    }])
+    .then((answers)=>{
+      const amount = answers['amount'] //salvando a quantidade de dinheiro que o usuário deseja depositar
+
+      //adicionando o valor
+      addAmount(accountName, )
+      operation()
+    })
+    .catch(err=>console.log(err))//pegando o erro e imprindo ele caso exista 
+
+  })
+  .catch(err => console.log(err)) //capiturando erro e imprimindo caso ocorra algum erro
+}
+
+// função que verifica se a conta existe
+function checkAccount(accountName){
+   if(!fs.existsSync(`accounts/${accountName}.json`)){ // usando fs fazemos uma verificação se dentro do diretório accounts existe uma conta com o nome da accountName
+    console.log(chalk.bgRed("Essa conta não existe, escolha outro nome!")) //imprimindo mensagem de erro, falando que essa conta nao existe
+    return false //retorna falaso para dizer que a conta não existe
+   }
+
+   return true //caso tenha passado pela verificação quer dizer que a conta existe por isso retorna true
+}
+
+//adicionando o valor dentro da conta do usuário
+function addAmount(accountName, amount){
+  const account = getAccount(accountName)
+
+  if(!amount){
+    console.log(chalk.bgRed.black("Ocorreu um erro tente novamente mais tarde"))
+    return deposit()
+  }
+
+  accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+}
+
+// função para pegar uma conta
+function getAccount(accountName){
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,{ // criando uma variavel que utiliza o modulo fs que vai ler um arquivo dentro da pasta accounts com o nome da conta
+    encoding: 'utf8', // colocando o padrão brasileiro para aceitar acentos e ç
+    flag: 'r' //essa flag fala que eu só quero ler o arquivo
+  })
+
+  return JSON.parse(accountJSON) //o accountJSON estava lendo um arquivo entao estava vindo como texto mas com o JSON.parse ele esta convertendo o accountJSON para um JSON
 }
